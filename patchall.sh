d35c007723069dba7a91aae831240bb581055c27
ShevT <shev_t@mail.ru>
2013-11-23 11:54:41 +0400

    add CM_BUILDTYPE patch

--- patchall.sh
+++ patchall.sh
@@ -16,3 +16,8 @@ traffic/traffic.sh $DSTDIR
 echo ""
 echo "${grn}Applying WiFi Country patch${txtrst}"
 cat allpatches/WiFi_Country.patch | patch -d $DSTDIR/frameworks/opt/telephony -p1 -N -r -
+
+# Allowed to provide the name CM_BUILDTYPE
+echo ""
+echo "${grn}Allowed to provide the name CM_BUILDTYPE${txtrst}"
+cat allpatches/CM_BUILDTYPE.patch | patch -d $DSTDIR/vendor/cm -p1 -N -r -
