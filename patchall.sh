c97d0b9efbefb9dd096e41ba704bcdecb2843240
ShevT <shev_t@mail.ru>
2013-11-23 11:56:18 +0400

    add linaro 4.7 and linaro patch

--- patchall.sh
+++ patchall.sh
@@ -9,6 +9,9 @@ then
     exit 1
 fi
 
+grn=$(tput setaf 2) # Green
+txtrst=$(tput sgr0) # Reset
+
 # Traffic indicator patch
 traffic/traffic.sh $DSTDIR
 
@@ -21,3 +24,10 @@ cat allpatches/WiFi_Country.patch | patch -d $DSTDIR/frameworks/opt/telephony -p
 echo ""
 echo "${grn}Allowed to provide the name CM_BUILDTYPE${txtrst}"
 cat allpatches/CM_BUILDTYPE.patch | patch -d $DSTDIR/vendor/cm -p1 -N -r -
+
+# linaro toolchain
+echo ""
+echo "${grn}Copy linaro${txtrst}"
+cp -r linaro-4.7 $DSTDIR/prebuilts/gcc/linux-x86/arm/
+echo "${grn}Apply linaro patch${txtrst}"
+cat allpatches/linaro.patch | patch -d $DSTDIR/build -p1 -N -r -
